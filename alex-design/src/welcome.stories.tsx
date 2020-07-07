52747fd79587224fd44a79ec60cdc3329bffe40e
boyan <baiyanalways@163.com>
2020-07-07 23:58:39 +0800

    新增precommit和prepublish

--- alex-design/src/welcome.stories.tsx
+++ alex-design/src/welcome.stories.tsx
@@ -5,11 +5,11 @@ storiesOf('Welcome', module)
   .add('Welcome', () => {
     return (
       <>
-        <h1>欢迎来到 alex-ui 组件库</h1>
-        <p> alex-ui组件库是由 React + TypeScript 打造的</p>
+        <h1>欢迎来到 alex-design 组件库</h1>
+        <p> alex-design组件库是由 React + TypeScript 打造的</p>
         <h3>安装试试</h3>
         <code>
-          npm install alexui --save
+          npm install alex-design --save
         </code>
       </>
     )
